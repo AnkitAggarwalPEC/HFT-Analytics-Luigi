a84fae86d204e297e3018d28cb2ae08fa0fa1628
AnkitAggarwalPEC <ankit.ankit.aggarwal@gmail.com>
2017-08-01 22:36:41 +0530

    First Commit

---
+++ sunny/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Logs and TBT Analytics on steriods"""
+
+from sunny import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ sunny/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'sunny'
+project = "sunny"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Logs and TBT Analytics on steriods'
+authors = ['Ankit Aggarwal']
+authors_string = ', '.join(authors)
+emails = ['ankit.ankit.aggarwal@gmail.com']
+license = 'MIT'
+copyright = '2017 ' + authors_string
+url = "abc"
