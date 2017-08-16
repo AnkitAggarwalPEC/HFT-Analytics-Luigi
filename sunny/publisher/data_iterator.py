373f9cf7412eacaf63c9120f3161ff09f2847100
ankit.a <ankit.a@irage.in>
2017-08-16 18:19:20 +0530

    Data Iterator

---
+++ sunny/publisher/data_iterator.py
@@ -0,0 +1,9 @@
+from abc import ABCMeta,abstractmethod,abstractproperty
+"""
+This is used to iterate any kind of the iterables like(Socket, Files)
+"""
+class BaseIterator(object):
+    def __init__(self, iterable_object = None):
+
+
+
